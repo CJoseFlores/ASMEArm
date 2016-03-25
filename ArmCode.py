8408ac40d4d388c3dcbab921c680a41cda4ac703
Carlos Flores <cflor083@fiu.edu>
2016-03-25 15:49:53 -0400

    Finished Motor Object

--- ArmCode.py
+++ ArmCode.py
@@ -10,62 +10,6 @@ DOWN = 0
 RIGHT = UP
 LEFT = DOWN
 
-motorOneInit = 18
-motorTwoInit = 11
-motorTreeInit = 21
-motorFourInit = 13
-motorFiveInit = 22
-
-############################################################################################
-
-motorOneLeft = 14
-motorOneRight = 15
-
-motorTwoLeft = 10
-motorTwoRight = 9
-
-motorTreeLeft = 16
-motorTreeRight = 20
-
-motorFourLeft = 19
-motorFourRight = 26
-
-motorFiveLeft = 17
-motorFiveRight = 27
-
-#############################################################################################
-
-GPIO.setup(motorOneInit, GPIO.OUT)
-GPIO.setup(motorTwoInit, GPIO.OUT)
-GPIO.setup(motorTreeInit, GPIO.OUT)
-GPIO.setup(motorFourInit, GPIO.OUT)
-GPIO.setup(motorFiveInit, GPIO.OUT)
-GPIO.setup(motorOneLeft, GPIO.OUT)
-GPIO.setup(motorOneRight, GPIO.OUT)
-GPIO.setup(motorTwoLeft, GPIO.OUT)
-GPIO.setup(motorTwoRight, GPIO.OUT)
-GPIO.setup(motorTreeLeft, GPIO.OUT)
-GPIO.setup(motorTreeRight, GPIO.OUT)
-GPIO.setup(motorFourLeft, GPIO.OUT)
-GPIO.setup(motorFourRight, GPIO.OUT)
-GPIO.setup(motorFiveLeft,GPIO.OUT)
-GPIO.setup(motorFiveRight,GPIO.OUT)
-######################################################
-GPIO.output(motorTreeInit,GPIO.HIGH)
-GPIO.output(motorTreeRight,GPIO.HIGH)
-time.sleep(.5)
-GPIO.output(motorTreeRight,GPIO.LOW)
-
-#GPIO.output(motorTwoInit,GPIO.HIGH)
-#GPIO.output(motorTwoLeft,GPIO.HIGH)
-#time.sleep(.2)
-#GPIO.output(motorTwoLeft,GPIO.LOW)
-#Left is Down for: 2
-#Right is Up for: 2
-#Left is Up for:
-#Right is Down for:
-
-
 class Motor:
     __ipin = None
     __upin = None
@@ -80,9 +24,24 @@ class Motor:
         GPIO.setup(ipin, GPIO.OUT)
         GPIO.setup(upin, GPIO.OUT)
         GPIO.setup(dpin, GPIO.OUT)
+        GPIO.output(ipin, GPIO.HIGH)
         self.__speed = 0
 
     def movemotor(self, direction):
         if (direction == 1):
+            GPIO.output(self.__upin, GPIO.HIGH)
+        elif(direction == 0):
+            GPIO.output(self.__dpin, GPIO.HIGH)
+        else:
+            return
 
-Motor m1(1,2,3)
+    def stopmotor(self):
+        GPIO.output(self.__upin, GPIO.LOW)
+        GPIO.output(self.__dpin, GPIO.LOW)
+        return
+
+m1 = Motor(18, 15, 14)
+m2 = Motor(11, 9, 10)
+m3 = Motor(21, 20, 16)
+m4 = Motor(13, 26, 19)
+m5 = Motor(22, 27, 17)
