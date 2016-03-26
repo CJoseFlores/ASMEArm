9b70a24d1c79febac081413dfe1866b81932c9ba
Carlos Flores <cflor083@fiu.edu>
2016-03-26 12:48:09 -0400

    Got the seperate files working

--- arms_module.py
+++ arms_module.py
@@ -5,11 +5,6 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-U = 1
-D = 0
-R = U
-L = D
-
 class Motor:
     __ipin = None
     __upin = None
@@ -41,14 +36,3 @@ class Motor:
         GPIO.output(self.__upin, GPIO.LOW)
         GPIO.output(self.__dpin, GPIO.LOW)
         return
-
-#InitPin,UpPin,DownPin
-#time.sleep(timeinseconds) -> Same as arduino Delay(timeinms)
-m1 = Motor(18, 15, 14)
-m2 = Motor(11, 9, 10)
-m3 = Motor(21, 20, 16)
-m4 = Motor(13, 6, 5)
-m5 = Motor(7, 25, 8)
-# DOWN is close
-# UP is open
-
