b1be8a024fe5931be27fe6bf920c3e8f98b752c4
Carlos Flores <cflor083@fiu.edu>
2016-03-30 08:45:08 -0400

    Sensors working! Testing a bit more

--- ArmCode.py
+++ ArmCode.py
@@ -9,7 +9,6 @@ from arms_module import Arm
 from arms_module import UltraSonicSensor
 
 GPIO.setmode(GPIO.BCM)#Sets the ping Numbering System
-GPIO.cleanup() #Defaults pins to input, used to not power-up with code
 GPIO.setwarnings(False)
 
 U = 1
@@ -19,7 +18,7 @@ L = D
 
 #InitPin,UpPin,DownPin
 #time.sleep(timeinseconds) -> Same as arduino Delay(timeinms)
-m1 = Motor(18, 15, 14)
+m1 = Motor(23, 15, 14) # changed from 18 to s3
 m2 = Motor(22, 27, 17) #changed from 11 to 22, and 9 to 27, and 10 to 17
 m3 = Motor(21, 20, 16)
 m4 = Motor(13, 6, 5)
@@ -33,6 +32,7 @@ arm1 = Arm(m1, m2, m3, m4, m5)
 #sensor1 = UltraSonicSensor(23, 24)
 
 #print (sensor1.measure())
-print(irdist.get_distance(0))
+print("Sensor 1's Distance: ", irdist.get_distance(0))
+print("Sensor 2's Distance: ", irdist.get_distance(1))
 
 
