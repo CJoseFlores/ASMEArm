2acbcfcbc3eb17003d65479b1397efa67e7e4ca5
Carlos Flores <cflor083@fiu.edu>
2016-03-30 06:35:24 -0400

    Testing Bit-blasting SPI

--- irdist.py
+++ irdist.py
@@ -1,9 +1,29 @@
 #This uses modified code credited to Jeremy Blythe at:
 #https://github.com/jerbly/Pi/blob/master/distance-screen.py
 import mcp3008
+import time
+import os
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+# change these as desired - they're the pins connected from the
+# SPI port on the ADC to the Cobbler
+SPICLK = 11
+SPIMISO = 9
+SPIMOSI = 10
+SPICS = 8
+# set up the SPI interface pins
+GPIO.setup(SPIMOSI, GPIO.OUT)
+GPIO.setup(SPIMISO, GPIO.IN)
+GPIO.setup(SPICLK, GPIO.OUT)
+GPIO.setup(SPICS, GPIO.OUT)
+
 
 #Reads from the mcp3008 chip, and outputs the calculated distance from the
 #SHARP IR Sensor
+
 def get_distance(mcp3008_ch):
     num_samples = 10
     r = []
