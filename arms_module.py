cbeff87bde7baff579d96394948a4b9055d140e0
Carlos Flores <cflor083@fiu.edu>
2016-03-30 20:08:17 -0400

    Removed all UltraSonicSensor Code

--- arms_module.py
+++ arms_module.py
@@ -48,59 +48,6 @@ class Motor:
         GPIO.output(self.__dpin, GPIO.LOW)
         return
 
-####################################################################################
-# The Sensor class takes modified code from Matt Hawkin's Ultrasonic_2.py located at:
-# http://www.raspberrypi-spy.co.uk/2013/01/ultrasonic-distance-measurement-using-python-part-2/
-#This class is working well, but this kind of sensor will not suit our purposes
-class UltraSonicSensor:
-    __start = None
-    __stop = None
-    __elapsed = None
-    __distance = None
-    __GPIO_T = None
-    __GPIO_E = None
-
-    def __init__(self, ep, tp):
-        self.__GPIO_E = ep
-        self.__GPIO_T = tp
-        GPIO.setup(self.__GPIO_T, GPIO.OUT)
-        GPIO.setup(self.__GPIO_E, GPIO.IN)
-        GPIO.output(self.__GPIO_T, False)
-        return
-
-    def measure(self):
-        # This function measures a distance
-        GPIO.output(self.__GPIO_T, True)
-        time.sleep(0.00001)
-        GPIO.output(self.__GPIO_T, False)
-
-        while GPIO.input(self.__GPIO_E) == 0:
-            self.__start = time.time()
-
-        while GPIO.input(self.__GPIO_E) == 1:
-            self.__stop = time.time()
-
-        self.__elapsed = (self.__stop) - (self.__start)
-        self.__distance = (self.__elapsed * 17150)
-        self.__distance = round(self.__distance, 2)
-
-        return self.__distance
-
-    def measure_average(self):
-        # This function takes 3 measurements and
-        # returns the average.
-        self.measure()
-        distance1 = self.__distance
-        time.sleep(0.1)
-        self.measure()
-        distance2 = self.__distance
-        time.sleep(0.1)
-        self.measure()
-        distance3 = self.__distance
-        self.__distance = distance1 + distance2 + distance3
-        self.__distance = distance / 3
-        return self.__distance
-###################################################################################
 class Arm:
     __m1= None
     __m2 = None
@@ -113,7 +60,6 @@ class Arm:
         self.__m2 = m2
         self.__m3 = m3
         self.__m4 = m4
-        self.__snsr = snsr
         return
 
     #This dconfig is based from the original plan.
@@ -143,10 +89,10 @@ class Arm:
         stopflag = 0
         while(stopflag == 0):
             if(irdist.get_distance(1)>5): #ch1 sensor is ontop of m1. If the sensor doesn't detect the arm part, the following if statements
-                if(self.__snsr.measure()<123123):
+                if(irdist.get_distance(2)<123123):
                     self.__m1.move(1) #m1 begins to move up if the sensor is too low to the ground.
                     self.__m2.stop()
-                elif(self.__snsr.measure()>123123):
+                elif(irdist.get_distance(2)>123123):
                     self.__m2.move(0) #m2 begins to move down if the sensor is too high from the ground.
                     self.__m1.stop()
             else:
@@ -173,10 +119,10 @@ class Arm:
         stopflag = 0
         while (stopflag == 0):
             if (irdist.get_distance(3) < 123123):#ch3 sensor is the sensor on top of the rover.
-                if (self.__snsr.measure() < 123123):
+                if (irdist.get_distance(2) < 123123):
                         self.__m1.move(0)  # m2 begins to move down if the sensor is too high to the ground.
                         self.__m2.stop()
-                elif (self.__snsr.measure() > 123123):
+                elif (irdist.get_distance(2) > 123123):
                         self.__m2.move(1)  # m3 begins to move up if the sensor is too low from the ground.
                         self.__m1.stop()
             else:
